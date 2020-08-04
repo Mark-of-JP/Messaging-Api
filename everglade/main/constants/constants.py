3fd53cf3196ff0fd44534fcd1a5fc8119cec384d
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2020-08-04 13:17:20 -0400

    Fixing credentials error

--- everglade/main/constants/constants.py
+++ everglade/main/constants/constants.py
@@ -5,7 +5,7 @@ if os.environ.get('IS_HEROKU', False):
     from everglade.main.constants.cred.config import config
     
     with open('everglade/main/constants/cred/credentials.json', 'w') as outfile:
-        json.dump({
+        cred = {
         "type": os.environ.get('CREDENTIALS_TYPE', None),
         "project_id": os.environ.get('CREDENTIALS_PROJECT_ID', None),
         "private_key_id": os.environ.get('CREDENTIALS_PRIVATE_KEY_ID', None),
@@ -15,7 +15,11 @@ if os.environ.get('IS_HEROKU', False):
         "auth_uri": os.environ.get('CREDENTIALS_AUTH_URI', None),
         "token_uri": os.environ.get('CREDENTIALS_TOKEN_URI', None),
         "auth_provider_x509_cert_url": os.environ.get('CREDENTIALS_AUTH_PROVIDER_X509_CERT_URL', None),
-        "client_x509_cert_url": os.environ.get('CREDENTIALS_CLIENT_X509_CERT_URL', None)}, outfile)
+        "client_x509_cert_url": os.environ.get('CREDENTIALS_CLIENT_X509_CERT_URL', None)}
+
+        print('POOGERINO')
+        print(cred)
+        json.dump(cred, outfile)
 
     credentials_path = 'everglade/main/constants/cred/credentials.json'
 else:
